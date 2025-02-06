286c03ffcd991b6a19fabca850d5a5c316579fbf
十载北林 <1143524493@qq.com>
2025-02-06 13:38:33 +0700

    update article

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "http://localhost:4321",
+  integrations: [tailwind(), sitemap()],
 });
